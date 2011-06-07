236819e15c955955f269e270101446afa3552093
Alan Maciel <alan.maciel@ubernetics.com>
2011-06-07 15:12:19 -0500

    Commit inicial

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#parking
+This is a project called parking.
+####[*2011 übernetics Agile Software*](http://ubernetics.com/)
+[more info](http://ubernetics.com/products/parking)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parking::Application.config.secret_token = '170f9b10d1f2db899bc5c17fe83cbd7c55dfc22a7684abd712366fdbc0f0ff73c1f7b8ad5c7491432009da4a252fafdd6d8835e71a451ee6133a3e36341c6a89'
